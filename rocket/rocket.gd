697180d863cd03a9b43d06beb1bf245e533df461
jeff <jeff@legnd.com>
2017-07-24 12:36:44 -0500

    new rotation stuff

--- rocket/rocket.gd
+++ rocket/rocket.gd
@@ -9,23 +9,30 @@ var acceleration = 1.5
 # Ball's speed.
 var player_speed = 200
 
-func move(speed, acc, delta):
-	current_speed.x = lerp(current_speed.x, speed, acc * delta)
-	set_linear_velocity(Vector2(current_speed.x, get_linear_velocity().y))
+# var current_rot = 0.0
+# var rot_speed = ???
+
+var rotSpeed = 0.1
+var rot = 0
+
+func rotate(delta):
+	var current_rot = get_rot()
+	current_rot += delta
+	set_rot(current_rot)
 
 func fly(speed, acc, delta):
 	current_speed.y = lerp(current_speed.y, speed, acc * delta)
 	set_linear_velocity(Vector2(get_linear_velocity().x, current_speed.y))
-	
+
 func _ready():
 	# Viewport width.
 	var width = get_viewport().get_rect().size.width # px
 	# Called every time the node is added to the scene.
 	set_pos(Vector2(width / 2, 300))
-	
-	set_process(true)
 
-func _process(delta):
+	set_fixed_process(true)
+
+func _fixed_process(delta):
 	if (Input.is_key_pressed(KEY_W)):
 		fly(-player_speed, acceleration, delta)
 		get_node("Exhaust").set_hidden(false)
@@ -33,8 +40,10 @@ func _process(delta):
 	else:
 		get_node("Exhaust").set_hidden(true)
 	if (Input.is_key_pressed(KEY_A)):
-		move(-player_speed, acceleration, delta)
-		print(get_linear_velocity())
+		rot = get_rot() + rotSpeed
+		set_rot(rot)
+		print(get_rot())
 	if (Input.is_key_pressed(KEY_D)):
-		move(player_speed, acceleration, delta)
-		print(get_linear_velocity())
+		rot = get_rot() - rotSpeed
+		set_rot(rot)
+		print(get_rot())
