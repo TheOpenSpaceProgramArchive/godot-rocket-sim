b8693142a2748242aeafa930728bc1d62ca8b481
jeff <jeff@legnd.com>
2017-07-24 21:39:02 -0500

    shits broke

--- rocket/rocket.gd
+++ rocket/rocket.gd
@@ -1,16 +1,22 @@
 extends RigidBody2D
 
-# Initial speed.
-var current_speed = Vector2(0.0, 0.0)
-
-# Ball's acceleration.
-var acceleration = 1.5
-
-# Ball's speed.
-var player_speed = 200
+# The exhaust animation.
+onready var exhaust = get_node("Exhaust")
 
 # The rocket's yaw control rotational period imparted by an internal gyrosope.
 var period = 2.0 # s
+
+# Ignition force vector.
+func force_vector(magnitude):
+	return Vector2(0.0, magnitude).rotated(get_rot() + PI)
+
+# Engine ignition.
+func ignite(delta):
+	if Input.is_key_pressed(KEY_W):
+		exhaust.set_hidden(false)
+		apply_impulse(exhaust.get_pos(), delta * force_vector(200.0))
+	else:
+		exhaust.set_hidden(true)
 
 # Yaw control.
 func yaw(delta):
@@ -19,23 +25,20 @@ func yaw(delta):
 	if Input.is_key_pressed(KEY_D):
 		set_rot(get_rot() - 2 * PI * delta / period) # rad
 
-func fly(speed, acc, delta):
-	current_speed.y = lerp(current_speed.y, speed, acc * delta)
-	set_linear_velocity(Vector2(get_linear_velocity().x, current_speed.y))
+# Draw the direction of the ingition force.
+func _draw():
+	var from = exhaust.get_pos()
+	var to = exhaust.get_pos() - force_vector(200.0)
+	draw_line(from, to, Color(0, 1, 0))
 
 func _ready():
 	# Viewport width.
 	var width = get_viewport().get_rect().size.width # px
-	# Called every time the node is added to the scene.
-	set_pos(Vector2(width / 2, 300))
-
+	# Place the rocket close to the surface.
+	set_pos(Vector2(width / 2, 470.0))
+	#
 	set_fixed_process(true)
 
 func _fixed_process(delta):
-	if Input.is_key_pressed(KEY_W):
-		fly(-player_speed, acceleration, delta)
-		get_node("Exhaust").set_hidden(false)
-		print(get_linear_velocity())
-	else:
-		get_node("Exhaust").set_hidden(true)
+	ignite(delta)
 	yaw(delta)
